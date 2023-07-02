d0a3658f0e8f4481fbc34ea9e474a7ba0ac39439
SajoyPaul <sajoypaul909@gmail.com>
2023-07-02 20:31:34 +0530

    System output

---
+++ Java Outputs/Systemoutput.java
@@ -0,0 +1,12 @@
+
+public class Systemoutput
+{
+    public static void main(String[] args) 
+    {
+        int x =12 ;
+        float y = 22.1f ;
+        String s= "Hello";
+
+     System.out.println("this is print \ninteger :"+x+"\nfloat :"+y+"\nString :"+s);   
+    }   
+}
