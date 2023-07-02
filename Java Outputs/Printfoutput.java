6639acd77326da6c2af42ba593ae08c0827dbf7f
SajoyPaul <sajoypaul909@gmail.com>
2023-07-02 20:33:20 +0530

    output format by printf

---
+++ Java Outputs/Printfoutput.java
@@ -0,0 +1,9 @@
+public class Printfoutput
+{
+    public static void main(String[] args)
+    {
+        int i = 100;
+        float f = 12.5f; 
+        System.out.printf("Printing value using fomate specifier int = %d ,float = %f\n Pi = %.2f",i,f,Math.PI);  
+    }    
+}
