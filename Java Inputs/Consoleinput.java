13a34f6cfd13217f0899f71daa4ac424626413bc
SajoyPaul <sajoypaul909@gmail.com>
2023-07-02 20:24:42 +0530

    Console input

---
+++ Java Inputs/Consoleinput.java
@@ -0,0 +1,8 @@
+public class Consoleinput 
+{
+    public static void main(String[] args)
+    {
+        String name = System.console().readLine();
+        System.out.println(name);    
+    }    
+}
