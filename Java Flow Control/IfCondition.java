c939bf328654825fa9805b2826b7e1b6cf5df8e2
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 23:03:30 +0530

    IF Conditional Statement

---
+++ Java Flow Control/IfCondition.java
@@ -0,0 +1,11 @@
+public class IfCondition
+{
+    public static void main(String[] args) 
+    {
+        int i = 10;
+        if (i<15)
+        {
+            System.out.println("Insdie If condition");
+        }
+    }    
+}
