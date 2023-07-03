d82afde69170ad0f2425bc6a2f1e4cf84bbaae3d
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 22:10:40 +0530

    Using + over ()

---
+++ Java Operators/UsingPlusOverBracket.java
@@ -0,0 +1,9 @@
+class UsingPlusOverBracket
+{
+public static void main(String[] args) 
+{
+    int a = 5 , b =299;
+    System.out.println("A+B : "+a+b);
+    System.out.println("A+B : "+(a+b));    
+}
+}
