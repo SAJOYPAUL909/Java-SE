d27d93d08bbb5595731ce2e70228322005d48322
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 19:14:58 +0530

    Precidency and Associativity of operators

---
+++ Java Operators/PrecidencyAssociativity.java
@@ -0,0 +1,10 @@
+public class PrecidencyAssociativity 
+{
+    public static void main(String[] args) 
+    {
+    int a = 10, b= 20, c = 0 , d = 40, e = 27;
+    
+    System.out.println("d*b+c-e/a+d ="+(a*c+d));
+    System.out.println("d*b+c-e/a+d ="+(d*b+c-e/a+d));
+    }    
+}
