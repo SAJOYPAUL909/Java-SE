977039de17205538625ce0208801e18b915943df
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 13:07:05 +0530

    Arithematic Operators

---
+++ Java Operators/ArithmaticOperators.java
@@ -0,0 +1,15 @@
+class ArithmaticOperators
+{
+    public static void main(String[] args) 
+    {
+        int a = 24;
+        int b = 11;
+        
+        System.out.println("Arithematic Operators -->");
+        System.out.println("Addition '+' A+B = "+(a+b));
+        System.out.println("Substraction '-' A-B = "+(a-b));
+        System.out.println("Multiplication '*' A X B = "+(a*b));
+        System.out.println("Division '/' A / B = "+(a/b));
+        System.out.println("Modulous '%' A%B = "+(a%b));
+    }
+}
