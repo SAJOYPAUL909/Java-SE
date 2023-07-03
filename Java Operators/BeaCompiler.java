9d99f6b9ef74606f218f1db57fb6f88136859aca
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 22:08:31 +0530

    Be a Compiler

---
+++ Java Operators/BeaCompiler.java
@@ -0,0 +1,10 @@
+public class BeaCompiler 
+{
+    public static void main(String[] args) 
+    {
+        int a = 20, b = 10 , c =60;
+        
+        a  = b+++c;
+        System.out.println("a :"+a+",b :"+b);
+    }    
+}
