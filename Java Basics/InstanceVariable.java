ad4d37f222dd0967e2161168fc83ec8634d6aebf
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 23:05:03 +0530

    Instance Variable

--- Java Basics/InstanceVariable.java
+++ Java Basics/InstanceVariable.java
@@ -3,13 +3,6 @@ class InstanceVariable
     public String k = "Sandeep is my friend";
     public int age = 12;
 
-    public InstanceVariable()
-    {
-        this.k = k;
-        this.age = age;
-
-    }
-    
     public static void main(String[] args)
     {
         InstanceVariable i = new InstanceVariable();  
