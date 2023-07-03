a1e7df82d57ca6910fdfe5fa0ad2b19d4170f622
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 22:22:29 +0530

    Wrapper class

---
+++ Java Basics/Wrapper.java
@@ -0,0 +1,19 @@
+public class Wrapper
+{
+    
+    public static void main(String[] args) 
+    {
+        //autoboxing
+        char c = 'a';
+        Character d = c;
+        System.out.println("D = "+d); 
+
+        //unboxing 
+        Integer i = 10;
+        int j = i;
+        System.out.println("j = "+j);
+        
+    }    
+
+
+}
