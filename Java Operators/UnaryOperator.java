2b819d690e025ac80735b0f23f5807672927dce6
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 13:11:55 +0530

    Unary Operators

---
+++ Java Operators/UnaryOperator.java
@@ -0,0 +1,19 @@
+public class UnaryOperator 
+{
+    public static void main(String[] args) 
+    {
+        int a = 10 ;
+        int b = 10;
+        System.out.println("Post increment a :"+(a++));
+        System.out.println("a = "+a);    
+        System.out.println("pre increment a :"+(++a));
+        System.out.println("a = "+a);   
+
+        System.out.println("Post Decrement a :"+(b--));
+        System.out.println("a = "+b);    
+        System.out.println("pre Decrement a :"+(--b));
+        System.out.println("a = "+b);
+
+
+    }    
+}
