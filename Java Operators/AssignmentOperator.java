fbc7a57dda7f8805eae63d9f9c41e37a69f8fa3d
SajoyPaul <sajoypaul909@gmail.com>
2023-07-03 13:09:36 +0530

    Assignment Operators

---
+++ Java Operators/AssignmentOperator.java
@@ -0,0 +1,17 @@
+class AssignmentOperator
+{
+    public static void main(String[] args) 
+    {
+        int a = 121;
+        System.out.println("a += 2 :"+(a+=2));
+        //System.out.println("a = "+a);
+        System.out.println("a -= 2 :"+(a-=2));
+        //System.out.println("a = "+a);
+        System.out.println("a *= 2 :"+(a*=2));
+        //System.out.println("a = "+a);
+        System.out.println("a /= 2 :"+(a/=2));
+        //System.out.println("a = "+a);
+        System.out.println("a %= 2 :"+(a%=2));
+        //System.out.println("a = "+a);       
+    }
+}
