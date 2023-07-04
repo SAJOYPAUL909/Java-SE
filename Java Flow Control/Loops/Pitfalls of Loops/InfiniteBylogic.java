4eedba6888bb6921ea776afe7f4535e4e37e1e1e
SajoyPaul <sajoypaul909@gmail.com>
2023-07-04 20:29:46 +0530

    Infinite loop by unocuring condition

---
+++ Java Flow Control/Loops/Pitfalls of Loops/InfiniteBylogic.java
@@ -0,0 +1,10 @@
+public class InfiniteBylogic 
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 5; i != 0; i -= 2)
+        {
+            System.out.println(i);
+        }
+    }    
+}
