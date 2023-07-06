f7788f9429932f974a9a2d78b7148997559d1fad
SajoyPaul <sajoypaul909@gmail.com>
2023-07-06 10:57:41 +0530

    Continue inside Nested Loop

---
+++ Java Flow Control/Loops/Nested Loops/ContinueInsideNestedLoop.java
@@ -0,0 +1,19 @@
+public class ContinueInsideNestedLoop 
+{
+    public static void main(String[] args) 
+    {
+         for (int i = 1; i <= 5; i++) 
+         {
+            for (int j = 1; j <= 3; j++) 
+            {
+                if (i == 3 && j == 2) 
+                {
+                    continue;
+                }
+
+                System.out.println(i + " * " + j);
+            }
+        }    
+
+    }
+}
