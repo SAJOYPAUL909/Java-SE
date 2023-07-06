1f726d7f0c3c1d50bc60532fde9e0eadc4c76ee7
SajoyPaul <sajoypaul909@gmail.com>
2023-07-06 22:01:48 +0530

    Methods not returning a value

---
+++ Java Flow Control/MethodNotReturningValue.java
@@ -0,0 +1,14 @@
+public class MethodNotReturningValue 
+{
+    void sum(int a,int b)
+    {
+        int sum = 0;
+        sum = (a + b) / 10;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) 
+    {
+        MethodNotReturningValue obj = new MethodNotReturningValue();
+        obj.sum(26,21);
+    }
+}
