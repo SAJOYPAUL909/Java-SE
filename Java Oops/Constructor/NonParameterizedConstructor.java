8f84af52f7e7cdc4bcee2146cc8ba68d4a0884cc
SajoyPaul <sajoypaul909@gmail.com>
2023-07-09 20:56:34 +0530

    Non-parameterized Constructor

---
+++ Java Oops/Constructor/NonParameterizedConstructor.java
@@ -0,0 +1,16 @@
+public class NonParameterizedConstructor 
+{
+
+
+    public NonParameterizedConstructor()
+    {
+        System.out.println("This is Non-parameterized Constructor ");
+    }
+
+public static void main(String[] args) 
+{
+    NonParameterizedConstructor obj = new NonParameterizedConstructor();
+    
+    System.out.println("This is main method ");
+}    
+}
