9e7f070582967b238a9f4010d045b5b748fe7f3b
SajoyPaul <sajoypaul909@gmail.com>
2023-07-11 17:04:30 +0530

    Encapsulation Example

---
+++ Java Oops/Encapsulation/EncapsulationEx.java
@@ -0,0 +1,43 @@
+class Person
+{
+    private String name;
+    private int age;
+ 
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+     public int getAge()
+    {
+        return age;
+    }
+
+
+}
+
+
+public class EncapsulationEx 
+{
+    public static void main(String[] args) 
+    {
+        Person obj = new Person();
+        obj.setName("Java");
+        obj.setAge(28);
+
+        System.out.println("Name : "+obj.getName());
+        System.out.println("Age : "+obj.getAge());
+
+
+    }    
+}
