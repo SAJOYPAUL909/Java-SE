9d6d195ef0c8b020878914360ebd93c335ec65a8
SajoyPaul <sajoypaul909@gmail.com>
2023-07-12 18:49:40 +0530

    Functional Interfaces

---
+++ Java Oops/Abstraction/Interfaces/FunctionalInterfaces.java
@@ -0,0 +1,15 @@
+public class FunctionalInterfaces 
+{
+    public static void main(String args[])
+    {
+        // create anonymous inner class object
+        new Thread(new Runnable() {
+            @Override 
+            public void run()
+            {
+                System.out.println("New thread created");
+            }
+        }).start();
+    }
+    
+}
