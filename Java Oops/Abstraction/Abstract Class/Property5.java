8965cbdac983d7764fd2901c2ade7b886c6c39b7
SajoyPaul <sajoypaul909@gmail.com>
2023-07-30 20:48:26 +0530

    we can define static methods in an abstract class

---
+++ Java Oops/Abstraction/Abstract Class/Property5.java
@@ -0,0 +1,15 @@
+abstract class Helper
+{
+    static void fun()
+    {
+        System.out.println("Static method called in a abstract class");
+    }
+}
+
+public class Property5 
+{
+    public static void main(String[] args) 
+    {
+        Helper.fun();
+    }    
+}
