d451be2268cd307c96ec2a1b3b88ca7933659cfd
SajoyPaul <sajoypaul909@gmail.com>
2023-08-12 11:08:56 +0530

    Final methods can not be overridden

---
+++ Java Methods/Method Overriding/Property2.java
@@ -0,0 +1,22 @@
+class A
+{
+    final void show()
+    {}
+}
+
+class B extends A
+{
+    void show ()
+    {
+
+    }
+}
+
+public class Property2 
+{
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
+
