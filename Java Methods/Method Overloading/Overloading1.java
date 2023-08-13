a4caecb66cb3a3dcfc8dfd217339a9981eb91800
SajoyPaul <sajoypaul909@gmail.com>
2023-08-13 07:35:12 +0530

    Changing the Number of Parameters

---
+++ Java Methods/Method Overloading/Overloading1.java
@@ -0,0 +1,29 @@
+class Addition
+{
+    public int sum (int a , int b)
+    {
+        return  a+b;
+    }
+
+    public int sum (int a , int b , int c)
+    {
+        return  a+b+c;
+    }
+}
+
+
+public class Overloading1 
+{
+
+
+    public static void main (String args [])
+    {
+        Addition a = new Addition();
+        int add1 = a.sum(12, 23);
+        System.out.println("Sum of Two integer : "+add1);
+
+        
+        int add2 = a.sum(12, 23,6);
+        System.out.println("Sum of Two integer : "+add2);
+    }    
+}
