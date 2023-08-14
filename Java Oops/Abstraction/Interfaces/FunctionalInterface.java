41fe0ac1ad19bcb507143ebcaa0db7f09782cc5f
SajoyPaul <sajoypaul909@gmail.com>
2023-08-14 14:38:36 +0530

    Functional Interface in Java implemented by lambda function

---
+++ Java Oops/Abstraction/Interfaces/FunctionalInterface.java
@@ -0,0 +1,17 @@
+public class FunctionalInterface 
+{
+    public static void main(String[] args) 
+    {
+        new Thread(new Runnable()
+        {
+            public void run ()
+            {
+                System.out.println("Thread created ");
+            }
+        }
+            
+        ).start();
+
+        System.out.println("Functional Interface");
+    }    
+}
