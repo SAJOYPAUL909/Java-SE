91d1ea3841589ec9d8fb8b0729ad17ae48844e46
SajoyPaul <sajoypaul909@gmail.com>
2023-08-19 11:27:54 +0530

    Explicitly assigned strings

---
+++ Java Strings/Strings/ExplicitlyString.java
@@ -0,0 +1,9 @@
+public class ExplicitlyString 
+{
+    public static void main(String[] args) 
+    {
+        String s = "Sandeep";
+        s = s.concat("Patel");
+        System.out.println(s);    
+    }    
+}
