9ac1ebfd022350e998a4514de1dbf75b3b834b5e
SajoyPaul <sajoypaul909@gmail.com>
2023-08-19 10:34:46 +0530

    example of a String in Java

---
+++ Java Strings/StringExample.java
@@ -0,0 +1,8 @@
+public class StringExample 
+{
+    public static void main(String[] args) 
+    {
+        String str = new String("This is String");
+        System.out.println(str);     
+    }
+}
